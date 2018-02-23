ca580ab52e6a9d853bc8051993413d95f4316992
Dr_Newbie <tf2baidonation@gmail.com>
2018-02-24 02:17:29 +0800

    Pulverizer Perk Deck ; 9
    More check

--- Pulverizer Perk Deck/Hooks/copdamage.lua
+++ Pulverizer Perk Deck/Hooks/copdamage.lua
@@ -2,7 +2,7 @@ local pulverizer_damage_melee = CopDamage.damage_melee
 function CopDamage:damage_melee(attack_data)
 	local is_ply = attack_data.attacker_unit and alive(attack_data.attacker_unit) and attack_data.attacker_unit == managers.player:player_unit() and managers.player:player_unit():character_damage() or nil
 	local _prec = managers.player:upgrade_value("player", "passive_pulverizer_damage_stack", 0)
-	if is_ply and _prec and attack_data and attack_data.damage then
+	if is_ply and is_ply._pulverizer_damage_stack_t and _prec and attack_data and attack_data.damage then
 		if TimerManager:game():time() > is_ply._pulverizer_damage_stack_t then
 			is_ply._pulverizer_damage_stack_s = 0
 		end
@@ -12,7 +12,7 @@ function CopDamage:damage_melee(attack_data)
 		is_ply._pulverizer_damage_stack_t = TimerManager:game():time() + 10
 	end
 	local res = pulverizer_damage_melee(self, attack_data)
-	if is_ply and _prec and res and res.type == "death" then
+	if is_ply and is_ply._pulverizer_damage_stack_t and _prec and res and res.type == "death" then
 		is_ply._pulverizer_damage_stack_s = math.clamp(is_ply._pulverizer_damage_stack_s + _prec*4, 0, 24)
 	end
 	return res
--- Pulverizer Perk Deck/Hooks/fpcameraplayerbase.lua
+++ Pulverizer Perk Deck/Hooks/fpcameraplayerbase.lua
@@ -1,13 +1,15 @@
 Hooks:PostHook(FPCameraPlayerBase, "spawn_melee_item", "pulverizer_MeleeOverhaulFPCameraPlayerBasePostSpawnMeleeItem", function(self)
-	local melee_entry = managers.blackmarket:equipped_melee_weapon()
-	local aligns = tweak_data.blackmarket.melee_weapons[melee_entry].align_objects or {"a_weapon_left", "a_weapon_right"}
-	for k , v in ipairs(aligns) do
-		self._hand_effects = self._hand_effects or {}
-		self._hand_effects[k] = World:effect_manager():spawn({
-			effect = Idstring("effects/particles/fire/small_light_fire"),
-			parent = self._parent_unit:camera()._camera_unit:get_object(Idstring(v))
-		})
-	end
+	if managers.player:has_category_upgrade("player", "passive_pulverizer_melee_event", 0) then
+		local melee_entry = managers.blackmarket:equipped_melee_weapon()
+		local aligns = tweak_data.blackmarket.melee_weapons[melee_entry].align_objects or {"a_weapon_left", "a_weapon_right"}
+		for k , v in ipairs(aligns) do
+			self._hand_effects = self._hand_effects or {}
+			self._hand_effects[k] = World:effect_manager():spawn({
+				effect = Idstring("effects/particles/fire/small_light_fire"),
+				parent = self._parent_unit:camera()._camera_unit:get_object(Idstring(v))
+			})
+		end
+	end0
 end)
 
 Hooks:PostHook(FPCameraPlayerBase, "unspawn_melee_item", "pulverizer_MeleeOverhaulFPCameraPlayerBasePostUnspawnMeleeItem", function(self)
