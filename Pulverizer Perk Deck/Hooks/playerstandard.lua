b7bfb44e580fef7412fa2e3194de417afe7bffb8
Dr_Newbie <tf2baidonation@gmail.com>
2020-11-14 14:12:48 +0800

    Pulverizer Perk Deck[14]

--- Pulverizer Perk Deck/Hooks/playerstandard.lua
+++ Pulverizer Perk Deck/Hooks/playerstandard.lua
@@ -65,16 +65,16 @@ Hooks:PostHook(PlayerStandard, "_do_melee_damage", "pulverizer_melee_event", fun
 					hit_unit:character_damage():damage_fire({
 						variant = "fire",
 						damage = 1,
-						weapon_unit = weapon_unit,
-						attacker_unit = user_unit,
+						weapon_unit =  player_unit:inventory()._available_selections[1].unit,
+						attacker_unit = player_unit,
 						col_ray = col_ray,
 						armor_piercing = true,
 						fire_dot_data = {
-							dot_trigger_chance = "100",
-							dot_damage = "10",
-							dot_length = "3.1",
-							dot_trigger_max_distance = "3000",
-							dot_tick_period = "0.5"
+							dot_trigger_chance = 100,
+							dot_damage = 10,
+							dot_length = 3.1,
+							dot_trigger_max_distance = 3000,
+							dot_tick_period = 0.5
 						}
 					})
 				end
